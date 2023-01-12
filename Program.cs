593af19a9ba2fbece3db9c7b5366329f10e22be6
Francys Rodrigues <119719373+BLACKBARGS@users.noreply.github.com>
2023-01-12 00:11:54 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            string? operacao;
+
+            Console.WriteLine("Digite o primeiro número:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a operação desejada, depois aperte ENTER duas vezes para obter o resultado (+, -, *, /):");
+            operacao = Console.ReadLine();
+            if (Console.ReadLine() is null)
+                return;
+
+            switch (operacao)
+            {
+                case "+":
+                    Console.WriteLine("Resultado: " + (num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine("Resultado: " + (num1 - num2));
+                    break;
+                case "*":
+                    Console.WriteLine("Resultado: " + num1 * num2);
+                    break;
+            }
+        }
+    }
+}
